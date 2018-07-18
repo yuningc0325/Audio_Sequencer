a910f8651372e8498f05dadcca91265918c113e4
Yu-Ning Chang <yxc784@cs.bham.ac.uk>
2018-07-18 23:37:59 +0000

    1. store clicked notes in an array 2. play harmonic

--- client/js/audio.js
+++ client/js/audio.js
@@ -14,9 +14,17 @@
  *      3.4 connect all together in order.
  */
 	 
- 	 // store notes sound from mongoDB
+ 	 // notes from mongoDB
  	var bufferList= new Array(21);
+ 	//  store selected notes in an array
+ 	var selectedBufferList =new Array(16);
+ 	for(var i=0;i<16;i++){
+ 		selectedBufferList[i]=[];
+ 	}
+ 	
  	var context;
+ 	
+ 	
  	// make sure web audio API can be used in different browser
  	function checkUsable(){
  		var contextClass = (window.AudioContext || window.webkitAudioContext || window.mozAudioContext || window.oAudioContext || window.msAudioContext);
@@ -64,14 +72,28 @@
 		    for(var j=3;j>0;j--){
 		        notesArray.forEach(function(el,k){
 		            var indexOfJ=j;
+		            var indexOfI=i-1;
 		            $('#'+i+'_'+j+'_'+el.notes).on('click',function(){
+		            	  var sound= bufferList[k+21-7*indexOfJ];
 		            	  // play sound 
-		                  playSound(bufferList[k+21-7*indexOfJ]);
+		                  playSound(sound);
+		                  // store notes in an array ** should be changed to queue for efficiency reason.
+		                  // if division is not selected then adding the note to array, otherwise remove it 
+		                  if($(this).attr('class')=="notesContext notesContextClick"){
+		                  	 selectedBufferList[indexOfI].pop(sound);
+		                  }else{
+		                  	selectedBufferList[indexOfI].push(sound);
+		                  }
+		                 
 		            })
 		        })
 		    }
 		}
 	}
+	
+	
+	
+	
 	
 	// Every units on board can be avtived with given style
 	function boardActived(){
@@ -81,6 +103,8 @@
 		            var indexOfJ=j;
 		            $('#'+i+'_'+j+'_'+el.notes).on('click',function(){
 		                  $(this).toggleClass('notesContextClick');
+		                  // store clicked notes in a queue
+		                  
 		            })
 		        })
 		    }
@@ -89,3 +113,70 @@
 	
 	
 	
+							
+	// function playSeveralBuffer(){
+	// 	for(var i=0;i<10;i++)
+	// 	playSound(bufferList[i]);
+		
+	// }		
+	
+// play sounds together	
+
+// var bufferLoader;
+
+// function init() {
+// bufferLoader = new BufferLoader( context,
+//         [
+//           '/sound/piano/c2.mp3',
+// 		  '/sound/piano/e2.mp3',
+//         ],
+//         finishedLoading
+// 		);
+//     bufferLoader.load();
+// }
+
+// this function is used for playing notes in the same time, for example, chord.
+function playSounds(bufferArr) {
+	let numOfSource=bufferArr.length;
+	// Create two sources and play them both together. 
+	for(var i=0;i<numOfSource;i++){
+		 this['source'+i]=context.createBufferSource();
+		 this['source'+i].buffer=bufferArr[i];
+		 this['source'+i].connect(context.destination);
+		 this['source'+i].start(0);
+	}
+}
+	
+	
+	
+ $('.button-main-playback').on('click',function(){
+ 							  // Play one bar ** should play all bars in tempo
+ 							  // play note or chord in an order (bar1 to bar 16) ** it is supposed to play in a given tempo
+ 							  // The situation now is playing almost in the same time!!! lame
+ 								 setTimeout(function(){playSounds(selectedBufferList[0]);},500);
+ 							  	 setTimeout(function(){playSounds(selectedBufferList[1]);},1000);
+ 							  	 setTimeout(function(){playSounds(selectedBufferList[2]);},1500);
+ 							  	 setTimeout(function(){playSounds(selectedBufferList[3]);},2000);
+ 							  	 setTimeout(function(){playSounds(selectedBufferList[4]);},2500);
+ 							  	 setTimeout(function(){playSounds(selectedBufferList[5]);},3000);
+ 							  	 setTimeout(function(){playSounds(selectedBufferList[6]);},3500);
+ 							  	 setTimeout(function(){playSounds(selectedBufferList[7]);},4500);
+ 							  	 setTimeout(function(){playSounds(selectedBufferList[8]);},5000);
+ 							  	 setTimeout(function(){playSounds(selectedBufferList[9]);},5500);
+ 							  	 setTimeout(function(){playSounds(selectedBufferList[10]);},6000);
+ 							  	 setTimeout(function(){playSounds(selectedBufferList[11]);},6500);
+ 							  	 setTimeout(function(){playSounds(selectedBufferList[12]);},7000);
+ 							  	 setTimeout(function(){playSounds(selectedBufferList[13]);},7500);
+ 							  	 setTimeout(function(){playSounds(selectedBufferList[14]);},8000);
+ 							  	 setTimeout(function(){playSounds(selectedBufferList[15]);},8500);
+							})
+
+
+
+
+
+
+
+
+
+
